624e717ae31c07a13f5213a7a0249620d771ec8d
Vh.Koiiin <145177114+Koiiin@users.noreply.github.com>
2024-03-10 13:16:33 +0700

    Update SortC.cpp

--- SortC.cpp
+++ SortC.cpp
@@ -2,21 +2,25 @@
 #define MAXn 1000000
 
 using namespace std;
-double *a = new double[MAXn];
+double *arr = new double[MAXn];
 
 int main()
 {
-    cout<<"[Sort Function in C++] \n";
-    string dau = "DATA", cuoi = ".TXT";
-    for (int k = 1; k <= 10; k++)
+    cout<<"C++ Sort: " << '\n';
+    string HEAD = "DATA";
+    string TAIL = ".TXT";
+    for (int k = 1; k <= 10; ++k)
     {
-        string giua = to_string(k), file = dau + giua + cuoi;
-        ifstream nhap(file);
-        for (int i = 0; i < MAXn; i++) nhap>>a[i];
+        string BODY = to_string(k);
+        string file = HEAD + BODY + TAIL;
+        ifstream Input(file);
+        for (int i = 0; i < MAXn; i++) {
+            Input >> arr[i];
+        }
         clock_t start = clock();
-        sort(a,a+MAXn);
+        sort(arr, arr+MAXn);
         clock_t end = clock();
         double time_elapsed = double(end - start);
-        cout << "DATA " << k << ": " << time_elapsed << " ms" << "\n";
+        cout << "C++ sort " << k << " : " << time_elapsed << " ms" << "\n";
     }
 }
